6582b7da2ca7d79bae8a2a97a615dd62217ffe9d
YadaCoder <yadacoderr@gmail.com>
2025-02-02 23:03:41 +0300

    added telegram as a provider option

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -1,7 +1,7 @@
 export interface OTPiqConfig {
     apiKey: string;
 }
-export type SMSProvider = "auto" | "sms" | "whatsapp";
+export type SMSProvider = "auto" | "sms" | "whatsapp" | "telegram";
 export type SMSType = "verification" | "custom";
 export type SMSStatus = "pending" | "delivered" | "failed";
 export interface SendSMSOptions {
